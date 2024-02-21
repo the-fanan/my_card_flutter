9a537669c3e6afadf741ef38a4d2f0af1cb40bc6
Fanan Dala <fanan.dala@yahoo.com>
2024-02-21 23:16:58 +0100

    feat: maximize column width without altering container widths

--- lib/main.dart
+++ lib/main.dart
@@ -14,13 +14,38 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(
-            height: 100,
-            width: 100,
-            margin: const EdgeInsets.all(20),
-            color: Colors.white,
-            padding: const EdgeInsets.all(20),
-            child: const Text('Hello'),
+          child: Column(
+            // main axis size basically says cover entire vertical height or just height of children
+            mainAxisSize: MainAxisSize.max,
+            // determines how to order children. down is default, top to bottom
+            verticalDirection: VerticalDirection.down,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 100,
+                width: 100,
+                margin: const EdgeInsets.only(top: 10, bottom: 10),
+                color: Colors.white,
+                child: const Text('Container 1'),
+              ),
+              Container(
+                height: 100,
+                width: 100,
+                margin: const EdgeInsets.only(top: 10, bottom: 10),
+                color: Colors.blue,
+                child: const Text('Container 2'),
+              ),
+              Container(
+                height: 100,
+                width: 100,
+                margin: const EdgeInsets.only(top: 10, bottom: 10),
+                color: Colors.red,
+                child: const Text('Container 3'),
+              ),
+              Container(
+                width: double.infinity,
+              )
+            ],
           ),
         ),
       ),
